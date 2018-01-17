8e1f2764b5339afab1b0ba2e4ba807d49d4de653
redmitry@list.ru <redmitry@list.ru>
2018-01-17 14:57:06 +0100

    build dependencies checker

---
+++ java/elixibilitas-metrics-checker/src/main/java/es/elixir/bsc/openebench/checker/biotools/BuildDependenciesChecker.java
@@ -0,0 +1,72 @@
+/**
+ * *****************************************************************************
+ * Copyright (C) 2018 ELIXIR ES, Spanish National Bioinformatics Institute (INB)
+ * and Barcelona Supercomputing Center (BSC)
+ *
+ * Modifications to the initial code base are copyright of their respective
+ * authors, or their employers as appropriate.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ *****************************************************************************
+ */
+
+package es.elixir.bsc.openebench.checker.biotools;
+
+import es.elixir.bsc.elixibilitas.model.metrics.Build;
+import es.elixir.bsc.elixibilitas.model.metrics.Metrics;
+import es.elixir.bsc.elixibilitas.model.metrics.Project;
+import es.elixir.bsc.elixibilitas.model.metrics.Summary;
+import es.elixir.bsc.elixibilitas.model.metrics.Support;
+import es.elixir.bsc.openebench.checker.MetricsChecker;
+import es.elixir.bsc.openebench.model.tools.Dependencies;
+import es.elixir.bsc.openebench.model.tools.Tool;
+import java.net.URI;
+import java.util.List;
+
+/**
+ * @author Dmitry Repchevsky
+ */
+
+public class BuildDependenciesChecker implements MetricsChecker {
+    @Override
+    public Boolean check(Tool tool, Metrics metrics) {
+        Boolean bool = check(tool);
+        Project project = metrics.getProject();
+        Build build;
+        if (project == null) {
+            project = new Project();
+            project.setBuild(build = new Build());
+            metrics.setProject(project);
+        } else {
+            build = project.getBuild();
+            if (build == null) {
+                project.setBuild(build = new Build());
+            }
+        }
+        build.setDependencies(bool);
+        return bool;
+    }
+    
+    private static Boolean check(Tool tool) {
+        Dependencies dependencies = tool.getDependencies();
+        if (dependencies == null) {
+            return null;
+        }
+        
+        final List<URI> list = dependencies.getBuildDependencies();
+        return list != null && list.size() > 0;
+    }
+}
