8d6bf516305581b03f0ab3e13e1c9b954fb94a9d
redmitry@list.ru <redmitry@list.ru>
2018-06-27 16:41:19 +0200

    web.copyright & web.license metrics check

---
+++ java/elixibilitas-metrics-checker/src/main/java/es/elixir/bsc/openebench/checker/biotools/HomepageLicenseChecker.java
@@ -0,0 +1,53 @@
+package es.elixir.bsc.openebench.checker.biotools;
+
+import es.elixir.bsc.elixibilitas.model.metrics.Metrics;
+import es.elixir.bsc.elixibilitas.model.metrics.Project;
+import es.elixir.bsc.elixibilitas.model.metrics.Website;
+import es.elixir.bsc.openebench.checker.MetricsChecker;
+import es.elixir.bsc.openebench.model.tools.Tool;
+import es.elixir.bsc.openebench.model.tools.Web;
+
+/**
+ * @author Dmitry Repchevsky
+ */
+
+public class HomepageLicenseChecker implements MetricsChecker {
+
+    @Override
+    public String getToolPath() {
+        return "/web/license";
+    }
+    
+    @Override
+    public String getMetricsPath() {
+        return "/project/website/license";
+    }
+    
+    @Override
+    public Boolean check(Tool tool, Metrics metrics) {
+        Boolean bool = check(tool);
+        Project project = metrics.getProject();
+        if (Boolean.TRUE.equals(bool)) {
+            Website website;
+            if (project == null) {
+                metrics.setProject(project = new Project());
+                project.setWebsite(website = new Website());
+            } else {
+                website = project.getWebsite();
+                if (website == null) {
+                    project.setWebsite(website = new Website());
+                }
+            }
+            website.setLicense(true);
+        } else if (project != null && project.getWebsite() != null) {
+            project.getWebsite().setLicense(bool);
+        }
+        return bool;
+
+    }
+    
+    private static Boolean check(Tool tool) {
+        final Web web = tool.getWeb();
+        return web != null && web.getLicense() != null && !web.getLicense().isEmpty();
+    }
+}
